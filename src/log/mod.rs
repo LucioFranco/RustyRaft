308401e690afac84d3f6089cc9b4e44d9ab8d9b6
Lucio Franco <lucio.franco@du.edu>
2017-03-23 09:44:47 -0400

    added more methods to Log trait

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -11,7 +11,23 @@ pub trait Log {
     /// Returns current term
     fn current_term(&self) -> Result<Term, Self::Error>;
 
+    /// Update current term
     fn set_current_term(&mut self, term: Term) -> Result<(), Self::Error>;
+
+    /// Get last candidate that state machine voted for
+    fn voted_for(&self) -> Result<Term, Self::Error>;
+
+    /// Update last candidate that statemachine voted for
+    fn set_voted_for(&mut self, candidate: Term) -> Result<(), Self::Error>;
+
+    /// Get latest log term
+    fn latest_term(&self) -> Result<Term, Self::Error>;
+
+    /// Get latest log index
+    fn latest_index(&self) -> Result<LogIndex, Self::Error>;
+
+    /// Get entry at index
+    fn entry(&self, index: LogIndex) -> Result<(Term, &[u8]), Self::Error>;
 
     /// Appends the provided entries, and returns any errors
     fn append_entries(&mut self, from: LogIndex, entries: &[(Term, &[u8])]) -> Result<(), Self::Error>;
